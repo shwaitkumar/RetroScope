e4396e14bc6924f91c40d5b9dd6ce8817d896810
shwaitkumar <my@pers.on.al>
2024-12-21 01:25:37 +0530

    Added constants file

---
+++ RetroScope/Config/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  RetroScope
+//
+//  Created by Shwait Kumar on 21/12/24.
+//
+
+import Foundation
+
+class Constants {
+    
+    static let apiKey = "4a5ee62e-d99e-49ca-8d3d-377e123eb89f"
+    
+}
