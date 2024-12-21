d5e8e676f4bc64d20fa7735c10903acff31fd0ae
shwaitkumar <my@pers.on.al>
2024-12-21 18:04:34 +0530

    Handled error alert UI better on different screen sizes

--- RetroScope/Views/RetroErrorAlertView.swift
+++ RetroScope/Views/RetroErrorAlertView.swift
@@ -12,50 +12,52 @@ struct RetroErrorAlertView: View {
     let dismiss: () -> Void
     
     var body: some View {
-        Rectangle()
-            .foregroundStyle(Color.royalSapphire.opacity(0.5))
-            .overlay {
-                VStack {
-                    Rectangle()
-                        .fill(.clear)
-                    
-                    ZStack {
+        GeometryReader { geometry in
+            Rectangle()
+                .foregroundStyle(Color.royalSapphire.opacity(0.5))
+                .overlay {
+                    VStack {
                         Rectangle()
-                            .fill(.ivoryGlow.opacity(0.8))
-                            .stroke(Color.royalSapphire, lineWidth: 3)
+                            .fill(.clear)
                         
-                        VStack(spacing: 32) {
-                            Text(error)
-                                .font(.subheadline)
-                                .fontDesign(.monospaced)
-                                .foregroundStyle(.royalSapphire)
-                                .multilineTextAlignment(.center)
+                        ZStack {
+                            Rectangle()
+                                .fill(.ivoryGlow.opacity(0.8))
+                                .stroke(Color.royalSapphire, lineWidth: 3)
                             
-                            HStack(spacing: 24) {
-                                Spacer()
+                            VStack(spacing: 32) {
+                                Text(error)
+                                    .font(.subheadline)
+                                    .fontDesign(.monospaced)
+                                    .foregroundStyle(.royalSapphire)
+                                    .multilineTextAlignment(.center)
                                 
-                                RetroButtonView(title: "Ok", color: .crimsonBlaze, action: {
-                                    dismiss()
-                                })
-                                
-                                Spacer()
-                            } //: HSTACK
-                            .frame(height: 44)
-                        } //: VSTACK
-                        .padding(.horizontal)
-                        .padding()
-                    } //: ZSTACK
-                    .visualEffect { content, proxy in
-                        content
-                            .hueRotation(Angle(degrees: proxy.frame(in: .global).origin.y / 10))
-                    }
-                    .frame(width: UIScreen.main.bounds.width * 0.8)
-                    
-                    Rectangle()
-                        .fill(.clear)
-                } //: VSTACK
-            }
-            .ignoresSafeArea(.all)
+                                HStack(spacing: 24) {
+                                    Spacer()
+                                    
+                                    RetroButtonView(title: "Ok", color: .crimsonBlaze, action: {
+                                        dismiss()
+                                    })
+                                    
+                                    Spacer()
+                                } //: HSTACK
+                                .frame(height: 44)
+                            } //: VSTACK
+                            .padding(.horizontal)
+                            .padding()
+                        } //: ZSTACK
+                        .visualEffect { content, proxy in
+                            content
+                                .hueRotation(Angle(degrees: proxy.frame(in: .global).origin.y / 10))
+                        }
+                        .frame(width: geometry.size.width * (UIDevice.current.userInterfaceIdiom == .phone ? 0.8 : 0.5))
+                        
+                        Rectangle()
+                            .fill(.clear)
+                    } //: VSTACK
+                }
+                .ignoresSafeArea(.all)
+        }
     }
 }
 
